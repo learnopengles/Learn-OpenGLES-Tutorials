980ea9c2b1ce1ce981f3760d8427253b29af547a
elect86 <elect86@gmail.com>
2017-08-06 11:19:18 +0200

    Lesson One

--- android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson1/LessonOneActivity.java
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson1/LessonOneActivity.java
@@ -7,54 +7,54 @@ import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
-public class LessonOneActivity extends Activity 
-{
-	/** Hold a reference to our GLSurfaceView */
-	private GLSurfaceView mGLSurfaceView;
+public class LessonOneActivity extends Activity {
+    /**
+     * Hold a reference to our GLSurfaceView
+     */
+    private GLSurfaceView mGLSurfaceView;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) 
-	{
-		super.onCreate(savedInstanceState);
-		
-		mGLSurfaceView = new GLSurfaceView(this);
+    private final boolean useKotlin = true;
 
-		// Check if the system supports OpenGL ES 2.0.
-		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
-		final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		if (supportsEs2) 
-		{
-			// Request an OpenGL ES 2.0 compatible context.
-			mGLSurfaceView.setEGLContextClientVersion(2);
+        mGLSurfaceView = new GLSurfaceView(this);
 
-			// Set the renderer to our demo renderer, defined below.
-			mGLSurfaceView.setRenderer(new LessonOneRenderer());
-		} 
-		else 
-		{
-			// This is where you could create an OpenGL ES 1.x compatible
-			// renderer if you wanted to support both ES 1 and ES 2.
-			return;
-		}
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
 
-		setContentView(mGLSurfaceView);
-	}
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
 
-	@Override
-	protected void onResume() 
-	{
-		// The activity must call the GL surface view's onResume() on activity onResume().
-		super.onResume();
-		mGLSurfaceView.onResume();
-	}
+            // Set the renderer to our demo renderer, defined below.
+            if (useKotlin)
+                mGLSurfaceView.setRenderer(new LessonOneRendererKt());
+            else
+                mGLSurfaceView.setRenderer(new LessonOneRenderer());
+        } else {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
 
-	@Override
-	protected void onPause() 
-	{
-		// The activity must call the GL surface view's onPause() on activity onPause().
-		super.onPause();
-		mGLSurfaceView.onPause();
-	}	
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
 }
